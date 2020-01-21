b4bc5972fd3a7fcca6a141dfe812a09c5c711c33
berkaysenkoylu <senkoylu_berkay@yahoo.com>
2020-01-21 20:45:48 +0300

    Updates

---
+++ precache-manifest.0854175b0ce4793479ae92df82c82fa0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf0da2e4baad3e553b5ad5946bbb5c1",
+    "url": "/quiz-game/index.html"
+  },
+  {
+    "revision": "d3b067add8fbb20236e8",
+    "url": "/quiz-game/static/css/main.91411888.chunk.css"
+  },
+  {
+    "revision": "0bd9a4d5c01caa65eb33",
+    "url": "/quiz-game/static/js/2.47b7f7d2.chunk.js"
+  },
+  {
+    "revision": "988f72b2c96c2e48ee691383a2ee3e9d",
+    "url": "/quiz-game/static/js/2.47b7f7d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3b067add8fbb20236e8",
+    "url": "/quiz-game/static/js/main.b3fceff3.chunk.js"
+  },
+  {
+    "revision": "9f6ab27e221ffd1d4ab4",
+    "url": "/quiz-game/static/js/runtime-main.8d5ec681.js"
+  },
+  {
+    "revision": "b31b1aee50a746e7955f8e8e6bda041e",
+    "url": "/quiz-game/static/media/arrow-down.b31b1aee.svg"
+  },
+  {
+    "revision": "21b6743e415a578b9d9cdf06c1fbaf30",
+    "url": "/quiz-game/static/media/check.21b6743e.svg"
+  },
+  {
+    "revision": "017fbe80c84afa40c590df6f20df8421",
+    "url": "/quiz-game/static/media/close.017fbe80.svg"
+  },
+  {
+    "revision": "ceb1eae434ab552a02866e852488ab58",
+    "url": "/quiz-game/static/media/hourglass-1.ceb1eae4.svg"
+  },
+  {
+    "revision": "766148564e16a4bcee92737111af38b5",
+    "url": "/quiz-game/static/media/hourglass-2.76614856.svg"
+  },
+  {
+    "revision": "e33852fa2c64246a0abd29b17556b3d6",
+    "url": "/quiz-game/static/media/hourglass-3.e33852fa.svg"
+  },
+  {
+    "revision": "03f40d3a8d479a105b4d693ae69e667c",
+    "url": "/quiz-game/static/media/hourglass-o.03f40d3a.svg"
+  },
+  {
+    "revision": "fc5da3335aaf6e93d5c4be8b497bd673",
+    "url": "/quiz-game/static/media/sprite.fc5da333.svg"
+  }
+]);
